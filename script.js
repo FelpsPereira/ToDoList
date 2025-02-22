617064579d1215016ed6cb74ecc2a099f205403b
FelipePereira392 <felipepereiraandrade392@gmail.com>
2025-02-21 22:27:41 -0300

    Bugs...

--- script.js
+++ script.js
@@ -29,18 +29,21 @@ addButton.addEventListener('click', ()=>{
     const newTask = (title,assunt) =>{
         addDisplay.style.display = 'none'
         Blur.style.display = 'none'
-        const div = document.createElement('div'),divButtons = document.createElement('div'),pTitle = document.createElement('p'),pAssunt = document.createElement('p'),divContent = document.createElement('div'),removeButton = document.createElement('button'),completedButton = document.createElement('button')
+        const div = document.createElement('div'),divButtons = document.createElement('div'),pTitle = document.createElement('p'),pAssunt = document.createElement('p'),divContent = document.createElement('div'),removeButton = document.createElement('button'),completedButton = document.createElement('button'), arrowButton = document.createElement('button')
 
         div.classList.add('newTask')
         divContent.classList.add('divContent')
+        divButtons.classList.add('divButtons')
         pTitle.classList.add('newTitleP')
         pAssunt.classList.add('newAssuntP')
         removeButton.classList.add('removeButton')
         completedButton.classList.add('completedButton')
+        arrowButton.classList.add('arrowButton')
         
         
         pTitle.textContent = title
-        pAssunt.textContent = assunt
+        pAssunt.innerHTML = 'Assunt below'
+        arrowButton.innerHTML = '<i class="fa-solid fa-arrow-down"></i>'
 
         if(title == ''){
             pTitle.textContent = 'Title'
@@ -48,25 +51,35 @@ addButton.addEventListener('click', ()=>{
         if (assunt == ''){
             pAssunt.textContent = 'Nothing'
         }
+
+        
         
         removeButton.innerHTML = '<i class="fa-solid fa-xmark"></i>'
-        completedButton.innerHTML = '<i class="fa-solid fa-pen"></i>'
+        completedButton.innerHTML = '<i class="fa-solid fa-check"></i>'
 
         div.appendChild(divContent)
         divContent.appendChild(pTitle)
         divContent.appendChild(pAssunt)
+        divContent.appendChild(arrowButton)
         div.appendChild(divButtons)
         divButtons.appendChild(removeButton)
         divButtons.appendChild(completedButton)
         tasks.appendChild(div)
+        
+        removeButton.addEventListener('click', () =>{
+            div.remove();
+        })
 
-         
+        arrowButton.addEventListener('click', () => {
+            div.classList.toggle('expanded');
+        });
 
         a.textContent = ''
         t.value = ''
     }
     newTask(t.value,a.textContent)
 })
+  
 
 
 const editableDiv = document.querySelector('#assunt')
